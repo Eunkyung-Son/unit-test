eef255e5b8bbe26de15bcd1b95e792f404341797
EunkyungSon <sek9510@naver.com>
2023-02-28 00:00:58 +0900

    [SETTING] modify to jest watch mode

--- src/test/add.test.js
+++ src/test/add.test.js
@@ -12,3 +12,6 @@ test("", () => {
  * jest config -> collectCoverage -> false
  * 성공한 정보만 나오며, 테스트 커버리지는 나오지 않음
  */
+
+/** package.json watchAll option => 작업하고 있는 파일이 변경될 때 마다 테스트 코드 실행 */
+/** package.json watch option => 변경되지 않은 테스트 코드는 실행하지 않음 (git) */
